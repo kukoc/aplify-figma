75d135ee1a37dc0ebe2d8a03198601c7fd417016
Aitor Echevarría <aechevarriaf@dxc.com>
2023-01-12 18:36:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import ActionCard from './ui-components/ActionCard';
+import amplifyLogo from "./images/amplify-logo.svg";
+
+const alertFun = () => { 
+  alert("Hello World");
+}
+
+const overrrideProps = {
+  "image": {
+      src: amplifyLogo
+  },
+  "Button":{
+      onClick: alertFun
+  }    
+}
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ActionCard overrides={overrrideProps}></ActionCard>
     </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import config from './aws-exports';
+import { Amplify } from 'aws-amplify';
+
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react';
+
+
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AmplifyProvider>
     <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
